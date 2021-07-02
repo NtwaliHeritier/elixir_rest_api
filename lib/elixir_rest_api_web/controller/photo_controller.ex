7e0fb05e66db62f504281f92214992f27e4d8355
NtwaliHeritier <heritierntwali@gmail.com>
2021-07-02 10:26:29 +0200

    add show action in photo controller

--- lib/elixir_rest_api_web/controller/photo_controller.ex
+++ lib/elixir_rest_api_web/controller/photo_controller.ex
@@ -2,6 +2,8 @@ defmodule ElixirRestApiWeb.PhotoController do
     use ElixirRestApiWeb, :controller
 
     def show("id" => id) do
-        
+        {:ok, response} = HTTPoison.get("https://jsonplaceholder.typicode.com/albums/#{id}/photos")
+        {:ok, photos} = Poison.decode(response.body)
+        render(conn, "show.json", photos: photos)
     end
 end
