86a8370f4f23ec566b7df90fdd6d045c89b73673
NtwaliHeritier <heritierntwali@gmail.com>
2021-07-02 10:24:22 +0200

    add package for fetching api and json manipulation

---
+++ lib/elixir_rest_api/application.ex
@@ -0,0 +1,32 @@
+defmodule ElixirRestApi.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      # Start the Telemetry supervisor
+      ElixirRestApiWeb.Telemetry,
+      # Start the PubSub system
+      {Phoenix.PubSub, name: ElixirRestApi.PubSub},
+      # Start the Endpoint (http/https)
+      ElixirRestApiWeb.Endpoint
+      # Start a worker by calling: ElixirRestApi.Worker.start_link(arg)
+      # {ElixirRestApi.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: ElixirRestApi.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  def config_change(changed, _new, removed) do
+    ElixirRestApiWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
---
+++ lib/elixir_rest_api_web/controller/photo_controller.ex
@@ -0,0 +1,7 @@
+defmodule ElixirRestApiWeb.PhotoController do
+    use ElixirRestApiWeb, :controller
+
+    def show("id" => id) do
+        
+    end
+end
